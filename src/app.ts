2f71a6727cc2ff2b1b66d79243d6f2089bcc563d
HimandriSharma <himandrisharma27@gmail.com>
2024-12-20 16:04:21 +0530

    feat: simple db post

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import express from "express";
 import { db } from "./db/db_connect";
+import { mySchemaUsers } from "./db/schema";
+import { v4 } from "uuid";
+import { eq } from "drizzle-orm";
 
-db.$client
 const app = express();
 
+app.use(express.json());
+
 const PORT = 8000;
+
+app.post("/create-user", async (req, res) => {
+  let id = v4();
+  await db.insert(mySchemaUsers).values({
+    id,
+    name: req.body.name,
+    bio: req.body.bio,
+  });
+  let [user] = await db
+    .select()
+    .from(mySchemaUsers)
+    .where(eq(mySchemaUsers.id, id));
+  res.send(user);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
